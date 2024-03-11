4d16fa85f2a3a6b5b8b64673baab54827c357404
jihye <thoth_0302@naver.com>
2024-03-11 10:48:11 +0900

    <feat> ItemType 수정 및 ItemEventType 추가

--- Assets/Scripts/Enum.cs
+++ Assets/Scripts/Enum.cs
@@ -4,11 +4,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //enum 모음 script
+[UGS(typeof(ItemEventType))]
+public enum ItemEventType
+{
+    Nomal, 
+    Event, 
+    Usable, //usable은 사용을 누르거나 클릭을 한번 더 하면 사라지면서 안에 숨겨진 아이템이 나오도록 설정
+    Interaction //상호작용이 가능한 아이템. 인벤토리에 들어가지 않음.
+}
 
 [UGS(typeof(ItemType))]
 public enum ItemType
 {
-    Nomal, Event
+    Photo, Object
 }
 
 public enum LocationEvent //아이템을 가지고 특정 장소에서 탐색하면 이벤트 발동되도록
