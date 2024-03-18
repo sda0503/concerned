c768d4e6dc90d63e24bed7a509da1616c1e40e3c
sda0503 <sda0503@naver.com>
2024-03-18 22:42:47 +0900

    feat - map 추가

---
+++ Assets/Scripts/PopupBtn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupBtn : MonoBehaviour
+{
+    
+    public void Close()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void Move(int num)
+    {
+        gameObject.SetActive(false);
+        transform.parent.gameObject.SetActive(false);
+    }
+}
