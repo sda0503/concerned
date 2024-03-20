215e768572a4772078d37a35b13c81bf1c46923a
BakGuno <129590082+BakGuno@users.noreply.github.com>
2024-03-20 21:17:06 +0900

    <Feat> 오브젝트 관리용 Interface & UIManager 초안 추가

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,10 +10,7 @@ public class GameManager : MonoBehaviour
 {
     private static GameManager instance;
 
-    private Information Playerinformation;
-
- //TODO : 일단은 New
-
+    private Information Playerinformation;  //TODO : 일단은 New
 
     #region 뭔지 몰라도 조건이 많을것같아서 만듬
     //TODO :관련된 UI들 값 변경할수있도록 연결할 것
@@ -74,7 +71,7 @@ public class GameManager : MonoBehaviour
     public void DateChange()
     {
         Playerinformation.date++;
-        OnDateChange?.Invoke(); //UIUpdate
+        OnDateChange?.Invoke(); //UIUpdate (날짜표시)
         if (Playerinformation.date == 30)
         {
             //TODO : 선택하는 씬으로 넘어가야됨
@@ -84,18 +81,21 @@ public class GameManager : MonoBehaviour
     public void DayTimeChange()
     {
         //TODO : DayTime 기준으로 저녁이면 집밖에 못가거나 강제로 보내거나 결정해야함.
-        if ((int)Playerinformation.dayTime == 2)
-        {
-            Playerinformation.dayTime = Information.DayTimeenum.Evening;
-        }
-        else Playerinformation.dayTime++;
-        OnDayTimeChange?.Invoke(); //UIUpdate
+
+        int daytime = (int)Playerinformation.dayTime + 1;
+        if (daytime ==4)
+            DateChange();
+        Playerinformation.dayTime = (Information.DayTimeenum)(daytime % 3);
+        //나머지연산자로 하면 if없이 순환시킬 수 있다.
+        
+        
+        OnDayTimeChange?.Invoke(); //UIUpdate //시간대 (조명, 휴대폰이나 다른 UI)
     }
 
-    public void PositionChange()
+    public void PositionChange(int PosID) //엑셀로 관리 : 기획자나 다른사람이 관리하기에 편함 협업에 유리 . 코드로 관리 : 개발자가 전체 관리하면 해도됨 근데 컴파일 되는거신경써야됨.
     {
-        //여튼 업데이트
-        OnPositionChange?.Invoke(); //UIUpdate
+        Playerinformation.position = PosID;
+        OnPositionChange?.Invoke(); //UIUpdate 아마 BG 바꾸는 용도로 사용될 듯.
     }
     
     
