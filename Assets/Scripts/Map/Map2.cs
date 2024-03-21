33a22714a0d3c33fde6d0fa0a62a54200c09fbb9
jihye <thoth_0302@naver.com>
2024-03-21 16:44:57 +0900

    <feat> map2 생성
    다이아로그 추가 필요함

---
+++ Assets/Scripts/Map/Map2.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Map2 : MonoBehaviour
+{
+    public List<Button> NPC;
+
+    private void Start()
+    {
+        //어떤 NPC를 눌러도 같은 채팅이 나오도록
+    }
+    
+}
