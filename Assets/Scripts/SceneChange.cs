f301e77aeabdb811aeacbffe9189ad77a91609e2
dmstlf1104 <75960436+dmstlf1104@users.noreply.github.com>
2024-03-22 19:15:23 +0900

    소리,화면 전환
    -소리 껐다,켰다
    - 옵션에서 게임시작화면으로 이동

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public void Option()
+    {
+        SceneManager.LoadScene("Option");
+    }
+
+    public void STARTScene()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+}
---
+++ Assets/Scripts/Sound/SoundController.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    public AudioSource bgm;
+    public GameObject soundOn;
+    public GameObject soundOff;
+    private bool muted = false;
+
+
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("muted"))
+        {
+            PlayerPrefs.SetInt("muted", 0);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
+
+        UpdateBtning();
+        AudioListener.pause = muted;
+        bgm.Play();
+    }
+
+    public void SoundBtnClick()
+    {
+        if (muted == false)
+        {
+            muted = true;
+            AudioListener.pause = true;
+        }
+        else
+        {
+            muted = false;
+            AudioListener.pause = false;
+        }
+        Save();
+        UpdateBtning();
+    }
+
+    private void UpdateBtning()
+    {
+        if (muted == false)
+        {
+            soundOn.SetActive(true);
+            soundOff.SetActive(false);
+        }
+        else
+        {
+            soundOn.SetActive(false);
+            soundOff.SetActive(true);
+        }
+    }
+
+    private void Load()
+    {
+        muted = PlayerPrefs.GetInt("muted") == 1;
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetInt("muted", muted ? 1 : 0);
+    }
+}
