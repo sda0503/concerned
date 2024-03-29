99133b0345380ddabc5cd20a738ad3508b77bd4d
jihye <thoth_0302@naver.com>
2024-03-29 17:15:25 +0900

    <fix> npc만 있는 맵은 script 제거

--- Assets/Scripts/Map/Map5.cs
+++ Assets/Scripts/Map/Map5.cs
@@ -18,28 +18,28 @@ public class map5 : MonoBehaviour
         {
             buttons[i].onClick.RemoveAllListeners();
             int n = i;
-            //buttons[i].onClick.AddListener(() => OnButtonClick(n));
+            buttons[i].onClick.AddListener(() => OnButtonClick(n));
         }
     }
 
 
-    // private void OnButtonClick(int index)
-    // {
-    //     switch (index)
-    //     {
-    //         case 1:
-    //             if (ItemManager.Instance.getItems.ContainsKey(33))
-    //             {
-    //                 Utility.Instance.OnClickToFindItem(15, GameManager.Instance.itemCanvas);
-    //             }
-    //             else Debug.Log("����ִ�.");
-    //             break;
-    //         case 2:
-    //             Utility.Instance.OnClickToFindItem(17, GameManager.Instance.itemCanvas);
-    //             break;
-    //         case 3:
-    //             Utility.Instance.OnClickToFindItem(33, GameManager.Instance.itemCanvas);
-    //             break;
-    //     }
-    // }
+    private void OnButtonClick(int index)
+    {
+        switch (index)
+        {
+            case 1:
+                if (ItemManager.Instance.getItems.ContainsKey(33))
+                {
+                    Utility.Instance.OnClickToFindItem(15, DataManager.instance.itemCanvas);
+                }
+                else Debug.Log("nnnn.");
+                break;
+            case 2:
+                Utility.Instance.OnClickToFindItem(17, DataManager.instance.itemCanvas);
+                break;
+            case 3:
+                Utility.Instance.OnClickToFindItem(33, DataManager.instance.itemCanvas);
+                break;
+        }
+    }
 }
