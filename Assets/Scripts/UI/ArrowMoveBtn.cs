c9fc2073273c77dbcd367139bdb1f365c4c988a8
sda0503 <sda0503@naver.com>
2024-04-12 16:12:19 +0900

    fix - 프리팹 수정

---
+++ Assets/Scripts/UI/ArrowMoveBtn.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowMoveBtn : MonoBehaviour
+{
+    [SerializeField]
+    public string ArrowName;
+
+}
